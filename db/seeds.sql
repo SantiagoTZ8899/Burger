12775f045d5223eb217ff12f8ac171d505286855
Santiago Tolosa Zegarra <santiagot-z@santiagos-mbp.home>
2019-10-28 22:44:55 -0500

    added initial files and created db in mysql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+insert into burgers (id, burger_name, devoured) values (1, cheeseburger, false);
+insert into burgers (id, burger_name, devoured) values (2, double cheeseburger, false);
+insert into burgers (id, burger_name, devoured) values (3, veggie burger, true);
+
+
