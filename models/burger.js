e79439c37f428a4f6d91a36e6667e3ae553433fd
SantiagoTZ8899 <santiago.tz89@gmail.com>
2019-11-01 22:27:26 -0500

    import/require orm.js and create const burger

--- models/burger.js
+++ models/burger.js
@@ -2,15 +2,22 @@ const orm = require("../config/orm.js");
 
 const burger = {
     select: function(cb) {
-        orm.select("burgers", function(results) {
-            cb(results);
+        orm.select("burgers", function(result) {
+            cb(result);
         });
     },
 
     create: function(cb) {
         orm.create("burgers", column, value, function(result) {
-            cb(res);
+            cb(result);
+        });
+    },
+
+    update: function(cb) {
+        orm.update("burgers", column, newVal, whereCol, whereVal, function(result) {
+            cb(result);
         });
     }
-
 }
+
+module.exports = burger;
